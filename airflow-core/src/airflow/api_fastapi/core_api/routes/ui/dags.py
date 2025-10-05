4c73a1c7e4428622205b0b8e17084a0ebdaf3480
dor-bernstein <160585775+dor-bernstein@users.noreply.github.com>
2025-10-05 18:25:55 +0300

    change to prints (#4)

--- airflow-core/src/airflow/api_fastapi/core_api/routes/ui/dags.py
+++ airflow-core/src/airflow/api_fastapi/core_api/routes/ui/dags.py
@@ -120,10 +120,9 @@ def get_dags(
 ) -> DAGWithLatestDagRunsCollectionResponse:
     """Get DAGs with recent DagRun."""
     # Fetch DAGs with their latest DagRun and apply filters
-    logger = logging.getLogger(__name__)
-
+    print("Current tags:", tags)
     if tags and 'ETL' in tags.tags_list:
-        logger.info(f'Getting DAGs with ETL Label at {datetime.now()}')
+        print(f'Getting DAGs with ETL Label at {datetime.now()}')
     query = generate_dag_with_latest_run_query(
         max_run_filters=[
             last_dag_run_state,
@@ -159,7 +158,7 @@ def get_dags(
 
     dags = [dag for dag in session.scalars(dags_select)]
     if tags and 'ETL' in tags.tags_list:
-        logger.info(f'Got DAGs with ETL Label at {datetime.now()}')
+       print(f'Got DAGs with ETL Label at {datetime.now()}')
 
     # Populate the last 'dag_runs_limit' DagRuns for each DAG
     recent_runs_subquery = (
@@ -201,7 +200,7 @@ def get_dags(
 
     recent_dag_runs = session.execute(recent_dag_runs_select)
     if tags and 'ETL' in tags.tags_list:
-        logger.info(f'Populated recent runs for ETL label at {datetime.now()}')
+        print(f'Populated recent runs for ETL label at {datetime.now()}')
 
     # Fetch pending HITL actions for each Dag if we are not certain whether some of the Dag might contain HITL actions
     pending_actions_by_dag_id: dict[str, list[HITLDetail]] = {dag.dag_id: [] for dag in dags}
